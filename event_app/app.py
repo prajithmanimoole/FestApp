1cac441a9f491c6db669f5bcce8652f4f33dd92f
prajithmanimoole <prajithm23@gmail.com>
2025-09-11 21:46:50 +0530

    Update app.py to use PORT environment variable

--- event_app/app.py
+++ event_app/app.py
@@ -1942,6 +1942,8 @@ app = create_app()
 
 if __name__ == '__main__':
     # Local dev run: python app.py
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
